b6d9cc15d9e54832bf02e6c6ebf759d4ec110698
namin-an <namin0202@gmail.com>
2022-04-10 13:10:18 +0900

    style: remove annotations

--- visualize_fig4.py
+++ visualize_fig4.py
@@ -1,9 +1,6 @@
 """
 Running this file makes visualizions of results in the paper.
 
-*Preview of this code:
-- Packages
-
 - Loading human and ANN data
 - Fig. 4a (Fig. S8a, Fig. S8b)
 - Fig. 4b
@@ -12,30 +9,22 @@ Running this file makes visualizions of results in the paper.
 
 
 #%%
-# Packages
-
 import datetime
-date = datetime.datetime.now()
-print(f'Today is Happy{date: %A, %d, %m, %Y}.', '\n')
-
 import os
 import math
 import random 
 import itertools 
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 from matplotlib.lines import Line2D
-
 import scipy
 import scipy as sp
 from scipy import stats
-from scipy.stats import norm, wilcoxon, linregress # norm.cdf와 norm.ppf(percent point function, inverse of cdf-percentiles)은 역함수 관계
+from scipy.stats import norm, wilcoxon, linregress
 import scipy.stats as sp
-
 from statannot import add_stat_annotation
 from PIL import Image
 import cv2 as cv
@@ -51,25 +40,19 @@ from sklearn.utils import shuffle
 type = 'opt' # 'opt' or 'elec'
 test_type = type
 
-# 사람 데이터
+# human data
 human_data = 'E:\\ANNA_INTERN\\Human_Exp\\211202'
-# human_data = 'C:\\Users\\user\\Desktop\\Human_Exp\\211124'
-# human_data = 'C:\\Users\\user\\Desktop\\Human_Exp\\211118'
-# human_data = '/content/drive/MyDrive/Human_Exp/211118'
-# sel_ppl = [92, 93] # 정현, 서연
 
 if type == 'opt':
-  sel_ppl = list(range(300, 309)) + list(range(400, 408)) + [611] # 18개
+  sel_ppl = list(range(300, 309)) + list(range(400, 408)) + [611] 
 elif type == 'elec': 
-  # sel_ppl = [499, 500, 502] + list(range(504, 509)) + list(range(602, 606)) + list(range(608, 612)) # 16개 (잘한 남자랑 못한 여자 제거)
-  sel_ppl = [499, 500, 502] + list(range(503, 509)) + list(range(602, 607)) + list(range(608, 612)) # 18개
+  # sel_ppl = [499, 500, 502] + list(range(504, 509)) + list(range(602, 606)) + list(range(608, 612)) 
+  sel_ppl = [499, 500, 502] + list(range(503, 509)) + list(range(602, 607)) + list(range(608, 612)) 
 
 human_df = pd.DataFrame()
 n = 9
 for i in range(1, 80*n+1, 80):
     try:
-      # temp_df = pd.read_csv(f'C:\\Users\\user\\Desktop\\211108_ANNA_main_test_{i}.csv') 
-      # temp_df = pd.read_csv(os.path.join(human_data, f'211116_ANNA_main_test_{i}.csv'))
       j = i+79
       temp_df = pd.read_csv(os.path.join(human_data, f'main_test({i}_{j}).xls.csv'))
       if i == 1:
@@ -80,12 +63,7 @@ for i in range(1, 80*n+1, 80):
     except:
       print(i)
 
-# temp_list = [f'선택_A_{p}' for p in range(i, n+1) ]
-# human_df = human_df[['유저식별아이디', '나이', '성별', '학력', '시력', *temp_list]]
 human_df = human_df[human_df['유저식별아이디'].isin(sel_ppl)]
-# human_df = human_df[human_df.index.isin([34, 37, 38])] # 윤서, 세인, 나민
-# new_sel_cols = [col for col in human_df.columns if col != 503 or col!= 506] # 결측치 제거
-# human_df = human_df[new_sel_cols]
 orig_human_df = human_df
 human_df = human_df.fillna(0)
 
@@ -99,12 +77,11 @@ human_df = human_df[sel_col]
 human_df.index = sel_ppl 
 human_df.columns = list(range(80*n))
 
-# 결측치(0) 확인 (x: 사람아이디 번호, 즉, 0이면 대답 안했거나 못했다는 의미)
+# check outliers (zeros)
 plt.hist(human_df.values, density=True)
 plt.show()
 
-# 머신 데이터 
-# answer_df = pd.read_csv(f'C:\\Users\\user\\Documents\\Namin\\210930_MCs_for_dev.csv')
+# machine data
 answer_df = pd.read_csv(f'E:\\ANNA_INTERN\\Human_Exp\\211105_QAs_for_Set0_CNN_SVC_4classes_partial.csv')
 
 act_per_list, pix_list, gs_list, par_list = [], [], [], []
@@ -128,9 +105,7 @@ answer_df = answer_df.T
 orig_answer_df = answer_df
 
 
-# 사람과 머신 데이터
-# human_df이랑 answer_df 합치기
-
+# both
 mer_df = pd.concat([human_df, answer_df], axis=0)
 mer_df = mer_df.T
 mer_df = mer_df.fillna(0)
@@ -140,7 +115,6 @@ mer_df
 
 #%%
 # Fig. 4a (Fig. S8a, Fig. S8b)
-# 여러가지 머신러닝 모델들의 성능
 
 test_type_list = [test_type] #['opt', 'elec']
 model_type1_list = ['', '', '', '', '', '', '', '', 'PCA', 'PCA'] #['PCA', 'PCA', '', '']
@@ -152,10 +126,10 @@ for test_type in test_type_list:
         model_type = model_type1 + model_type2
 
         high_df = pd.DataFrame()
-        for m in range(1): # 제일 첫 번째 set에 대해서만
+        for m in range(1): 
             for seed in seed_list:
                 data_path = f'E:\\ANNA_INTERN\\Question Banks AI Hub_final\\16classes\\set{m}\\seed{seed}'
-                preprocessed_data_path =  os.path.join(data_path, 'comb0') # 16 classes 는 1 comb 밖에 없음.
+                preprocessed_data_path =  os.path.join(data_path, 'comb0') 
 
                 high_analysis_path = os.path.join(preprocessed_data_path, f'High_Analysis_{test_type}')
                 try:
@@ -177,7 +151,7 @@ for test_type in test_type_list:
         high_df = high_df[['image', 'hit_rate']]
 
         old_high_df_cols = high_df.columns
-        new_high_df_cols = [f'{model_type}_' + col_name for (i, col_name) in enumerate(old_high_df_cols) if i != 0] # file_name 제외하고 나머지에 모델 이름 앞에 달아주기
+        new_high_df_cols = [f'{model_type}_' + col_name for (i, col_name) in enumerate(old_high_df_cols) if i != 0] 
         new_high_df_cols = [old_high_df_cols.tolist()[0], *new_high_df_cols]
         high_df.columns = new_high_df_cols
 
@@ -206,7 +180,6 @@ for test_type in test_type_list:
         elif model_type == 'PIXEL_LR':
             pixel_lr_high_df = high_df
 
-# 나중에 시각화할 순서
 model_list = [
               pixel_svc_high_df, pixel_lr_high_df,
               pca_svc_high_df, pca_lr_high_df,
@@ -217,7 +190,7 @@ model_name_list = ['PIXEL_SVC', 'PIXEL_LR',
                    'PCASVC', 'PCALR',
                    'CNN_SVC', 'CNN_LR',
                    'CNN_AlexNet2_SVC', 'CNN_AlexNet2',
-                   'CNN_VggNet2_SVC', 'CNN_VggNet2'] # 위와 순서 일치해야 함.
+                   'CNN_VggNet2_SVC', 'CNN_VggNet2'] 
 temp_high_df = model_list[0].merge(model_list[1], on='image')
 for i in range(2, len(model_list)):
     temp_high_df = temp_high_df.merge(model_list[i], on='image')
@@ -226,17 +199,15 @@ temp_high_df = temp_high_df.drop(temp_high_df.tail(1).index)
 temp_high_df = temp_high_df.set_index('image')
 temp_high_df.columns = model_name_list
 
-# Archetypal 사람들의 성능 + 여러가지 머신러닝 모델들의 성능
 
 temp_acc_df = mer_df3.copy()
 
 archetypal_df = temp_acc_df[sel_ppl]
-# archetypal_df = temp_acc_df[sel_ppl].mean(axis=1).to_frame()
-# archetypal_df.columns = ['Human']
 
 merged_df = pd.merge(archetypal_df, temp_high_df, left_index=True, right_index=True, how='inner')
 
 chosen_hyperpar = ''
+
 
 # Face-level
 
@@ -254,7 +225,6 @@ for file_name in merged_df.index:
 merged_df['face'] = temp_index
 merged_df['PIX'] = pix_list
 merged_df['hyperpar'] = h_list
-#merged_df = merged_df[merged_df['PIX'] == '64PIX']
 if chosen_hyperpar != '':
     new_merged_df = merged_df[merged_df['hyperpar'] == chosen_hyperpar]
 else:
@@ -262,8 +232,7 @@ else:
 
 temp_acc_df = new_merged_df.groupby('face').mean()
 
-# --------------- 사람 성능 순서대로 사진 배열 ---------------
-temp_df = temp_acc_df.sort_values([sel_ppl[0]]) # 16_hit_rate를 기준으로 difficult(성능 낮은)한 순서부터 easy(성능 높은) face ID 나열
+temp_df = temp_acc_df.sort_values([sel_ppl[0]]) # the highest human accuracy at the beginning
 
 color_index_dict = {k:[] for k in range(1, temp_df.shape[1]+1)}
 for (t, col) in enumerate(temp_df.columns):
@@ -290,25 +259,17 @@ for i, person in enumerate(temp_df.index):
                 ax[i][0].set_yticks([])
                 ax[i][0].set_aspect('equal')
             else:
-                # flipped_color = int(color_index_dict[j][i]) # j는 model의 개수이므로 dataframe의 column에 해당, i는 face-class의 개수이므로 dataframe의 index에 해당
-                # color = num_class-1 - flipped_color
-                # color = sns.color_palette('Spectral', num_class)[color]
                 
                 hit_rate = color_index_dict[j][i]
                 assert hit_rate >= 0 and hit_rate <= 1
                 num_val = 1000
-                # conv_hr = (max(temp_df.max()) - 1) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))  # M-1(빨) ~ M(파)
-                # conv_hr = min(temp_df.min()) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min())) # m(빨) ~ m+1(파)
-                # conv_hr = (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))# m(빨) ~ M(파) # 네모마다 상대적
-                conv_hr = hit_rate # 0(빨) ~ 1(파) # 절대적
-                conv_hr = conv_hr * (num_val - 1) # num_val 등분
+                # conv_hr = (max(temp_df.max()) - 1) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))  # M-1(red) ~ M(blue)
+                # conv_hr = min(temp_df.min()) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min())) # m(red) ~ m+1(blue)
+                # conv_hr = (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))# m(red) ~ M(blue) # relative values
+                conv_hr = hit_rate # 0(red) ~ 1(blue) # absolute values
+                conv_hr = conv_hr * (num_val - 1) # equalize 
                 conv_hr = round(conv_hr)
 
-                # if conv_hr < 0:
-                #     new_conv_hr = 0
-                # elif conv_hr >= num_val:
-                #     new_conv_hr = num_val-1
-                # else:
                 new_conv_hr = conv_hr
                 color = sns.color_palette('Spectral', num_val)[new_conv_hr] 
                 
@@ -337,8 +298,7 @@ merged_df['facepar'] = temp_index
 
 temp_acc_df = merged_df.groupby('facepar').mean()
 
-# --------------- 사람 성능 순서대로 사진 배열 ---------------
-temp_df = temp_acc_df.sort_values([sel_ppl[0]]) # 16_hit_rate를 기준으로 difficult(성능 낮은)한 순서부터 easy(성능 높은) face ID 나열
+temp_df = temp_acc_df.sort_values([sel_ppl[0]])
 
 color_index_dict = {k:[] for k in range(1, temp_df.shape[1]+1)}
 for (t, col) in enumerate(temp_df.columns):
@@ -364,19 +324,15 @@ for i, perpar in enumerate(temp_df.index):
                 ax[i][0].set_xticks([])
                 ax[i][0].set_yticks([])
                 ax[i][0].set_aspect('equal')
-            else:
-                # flipped_color = int(color_index_dict[j][i]) # j는 model의 개수이므로 dataframe의 column에 해당, i는 face-class의 개수이므로 dataframe의 index에 해당
-                # color = num_class-1 - flipped_color
-                # color = sns.color_palette('Spectral', num_class)[color]
-                
+            else:        
                 hit_rate = color_index_dict[j][i]
                 num_val = 1000
-                # conv_hr = (max(temp_df.max()) - 1) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))  # M-1(빨) ~ M(파)
-                # conv_hr = min(temp_df.min()) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min())) # m(빨) ~ m+1(파)
+                # conv_hr = (max(temp_df.max()) - 1) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))  # M-1(red) ~ M(blue)
+                # conv_hr = min(temp_df.min()) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min())) # m(red) ~ m+1(blue)
                 assert hit_rate >= 0 and hit_rate <= 1
-                # conv_hr = (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))# m(빨) ~ M(파)
-                conv_hr = hit_rate # 0(빨) ~ 1(파) # 절대적
-                conv_hr = conv_hr * (num_val - 1) # num_val 등분
+                # conv_hr = (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))# m(red) ~ M(blue)
+                conv_hr = hit_rate # 0(red) ~ 1(blue) # absolute
+                conv_hr = conv_hr * (num_val - 1) # equalize
                 conv_hr = round(conv_hr)
 
                 # if conv_hr < 0:
@@ -412,7 +368,7 @@ for (t, col) in enumerate(temp_df.columns):
 
 num_class = temp_df_par.shape[0] # 16*5
 
-# 부분적으로만 그려보자
+# subplots
 fig, ax = plt.subplots(num_class, temp_df_par.shape[1]+1, figsize=(2*(temp_df_par.shape[1]+1), 2*num_class)) # multiply 5 b/c there are 5 face-attributes per one face-class
 
 for i, perpar in enumerate(temp_df_par.index):
@@ -430,25 +386,13 @@ for i, perpar in enumerate(temp_df_par.index):
                 ax[i][0].set_yticks([])
                 ax[i][0].set_aspect('equal')
             else:
-                # flipped_color = int(color_index_dict[j][i]) # j는 model의 개수이므로 dataframe의 column에 해당, i는 face-class의 개수이므로 dataframe의 index에 해당
-                # color = num_class-1 - flipped_color
-                # color = sns.color_palette('Spectral', num_class)[color]
-                
                 hit_rate = color_index_dict[j][i]
                 num_val = 16
-                # conv_hr = (max(temp_df.max()) - 1) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))  # M-1(빨) ~ M(파)
-                # conv_hr = min(temp_df.min()) + (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min())) # m(빨) ~ m+1(파)
                 assert hit_rate >= 0 and hit_rate <= 1
-                # conv_hr = (hit_rate - min(temp_df.min())) / (max(temp_df.max()) - min(temp_df.min()))# m(빨) ~ M(파)
                 conv_hr = hit_rate
-                conv_hr = conv_hr * (num_val - 1) # num_val 등분
+                conv_hr = conv_hr * (num_val - 1) 
                 conv_hr = round(conv_hr)
 
-                # if conv_hr < 0:
-                #     new_conv_hr = 0
-                # elif conv_hr >= num_val:
-                #     new_conv_hr = num_val-1
-                # else:
                 new_conv_hr = conv_hr
                 color = sns.color_palette('Spectral', num_val)[new_conv_hr] 
                 
@@ -462,11 +406,11 @@ fig.subplots_adjust(wspace=0, hspace=0)
 plt.savefig(f'C:\\Users\\user\\Desktop\\face_partial_attribute_{test_type}.png')
 plt.show()
 
+
 #%%
 # Fig. 4b
 
-# 현재 몇 개 째 돌리고 있는지
-
+# check the current status
 test_type_list = ['elec'] #['opt', 'elec']
 model_type1_list = [''] #['PCA', 'PCA', '', '']
 model_type2_list = ['CNN_AlexNet2_SVC'] #['SVC', 'LR', 'CNN_LR', 'CNN_SVC']
@@ -476,10 +420,10 @@ gs_order_list = ['2GS', '4GS', '6GS', '8GS', '16GS']
 class_list = [4] #[2, 4, 16]
 
 for test_type in test_type_list:
-    for (model_type1, model_type2) in zip(model_type1_list, model_type2_list): # 제일 첫 번째 model에 대해서만
+    for (model_type1, model_type2) in zip(model_type1_list, model_type2_list): 
         model_type = model_type1 + '_' + model_type2
         for c in class_list:
-            for m in range(1): # 제일 첫 번째 set에 대해서만
+            for m in range(1): 
                 high_df = pd.DataFrame()
                 for seed in seed_list:
                 
@@ -501,18 +445,18 @@ gs_order_list = ['2GS', '4GS', '6GS', '8GS', '16GS']
 class_list = [4, 16] # [2, 4, 16]
 
 for test_type in test_type_list:
-    for (model_type1, model_type2) in zip(model_type1_list, model_type2_list): # 제일 첫 번째 model에 대해서만
+    for (model_type1, model_type2) in zip(model_type1_list, model_type2_list): 
         model_type = model_type1 + model_type2
 
         for c in class_list:
             high_df = pd.DataFrame()
-            for m in range(1): # 제일 첫 번째 set에 대해서만
+            for m in range(1): 
                 for seed in seed_list:
                     if c == 16 or (c == 4 and seed == 22): 
                         data_path = f'E:\\ANNA_INTERN\\Question Banks AI Hub_final\\{c}classes\\set{m}\\seed{seed}'
                         try:
                             for n in range(len(os.listdir(data_path))):
-                                preprocessed_data_path =  os.path.join(data_path, f'comb{n}') # 16 classes 는 1 comb 밖에 없음.
+                                preprocessed_data_path =  os.path.join(data_path, f'comb{n}') 
 
                                 high_analysis_path = os.path.join(preprocessed_data_path, f'High_Analysis_{test_type}')
                                 
@@ -539,7 +483,7 @@ for test_type in test_type_list:
                         pass
 
             old_high_df_cols = high_df.columns
-            new_high_df_cols = [f'{c}_' + col_name for (i, col_name) in enumerate(old_high_df_cols) if i == 2] # hit_rate에만 c 달아주기
+            new_high_df_cols = [f'{c}_' + col_name for (i, col_name) in enumerate(old_high_df_cols) if i == 2] 
             new_high_df_cols = [old_high_df_cols.tolist()[0], old_high_df_cols.tolist()[1], *new_high_df_cols]
             high_df.columns = new_high_df_cols
 
@@ -557,14 +501,13 @@ for test_type in test_type_list:
 high_df = high_df_4.merge(high_df_16, on=['actual_person', 'full_hyperpar'])
 high_df = high_df.astype({'actual_person':int})
 
-# --------- low-level (class별) ---------
+# --------- low-level (class) ---------
 
 df = high_df
 df = pd.DataFrame(df.groupby('actual_person').agg(np.mean)).reset_index()
 
-# --------------- 사람별 확인용 ---------------
 temp_df = df.set_index('16_hit_rate')
-temp_df = temp_df.sort_index(axis=0, ascending=True) # 16_hit_rate를 기준으로 difficult, easy face ID 찾기
+temp_df = temp_df.sort_index(axis=0, ascending=True) # find difficult, easy face-classes
 
 num_class = len(list(map(int, list(df[f'actual_person'].unique())))) # 16
 
@@ -585,7 +528,7 @@ for i in com_class_list:
     else:
         M, m = i[1], i[0]
 
-    # --------------- 사람별 확인용 ---------------
+    # --------------- for each face-class ---------------
     # def label_point(df, ax):
     #     for i, point in df.iterrows():
     #         ax.annotate(point['actual_person'], xy = (point[f'{M}_hit_rate'], point[f'{m}_hit_rate']), xytext=(2,-2), textcoords="offset points")
